48defb284022f0b412c7470284a8a58a8118af39
ShahzaibMunawar <ShahzaibMunawar@users.noreply.github.com>
2023-03-22 16:42:44 +0500

    53 - Creating dynamic custom buttons

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { CSSTransition } from "react-transition-group";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
 
-import "./SideDrawer.css";
+import './SideDrawer.css';
 
-const SideDrawer = (props) => {
+const SideDrawer = props => {
   const content = (
-    //      mountOnEnter, unmountOnExit used with the React Transition
-    //      mountOnEnter: This prop specifies whether the child component should be mounted on the DOM when the in prop (which controls whether the component is visible or not) changes from false to true. By default, this prop is set to false, meaning the child component will be mounted as soon as the parent component is mounted, regardless of the in prop.
     <CSSTransition
       in={props.show}
       timeout={200}
@@ -15,13 +13,11 @@ const SideDrawer = (props) => {
       mountOnEnter
       unmountOnExit
     >
-      <aside className="side-drawer" onClick={props.onClick}>
-        {props.children}
-      </aside>
+      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
     </CSSTransition>
   );
 
-  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
