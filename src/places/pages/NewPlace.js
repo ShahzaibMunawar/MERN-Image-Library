1bb8b62aceeab5a50113d921406dbc637df48010
ShahzaibMunawar <ShahzaibMunawar@users.noreply.github.com>
2023-03-25 23:48:43 +0500

    Input (Validation, State Management, useReducer,useEffect)
    did validation
    input state management like on touch, on input managing different states of the input
    
    use reducer to manage change state
    
    use effect for managing component value change

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
+
 import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators.js";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {});
+  const descriptionInputHandler = useCallback((id, value, isValid) => {});
+
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        element="testarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (Min Length is 5)."
+        onInput={descriptionInputHandler}
+      />
     </form>
   );
 };
